985fcf389214dcc0a88bdc15f20a13c330a563cc
Wekend <wekendmc@gmail.com>
2025-10-10 12:33:05 -0700

    Chat improvements complete!

---
+++ src/main/java/dev/wekend/mop/mixins/menu/ScreenCloseMixin.java
@@ -0,0 +1,28 @@
+package dev.wekend.mop.mixins.menu;
+
+import dev.wekend.mop.events.IgnoreCloseScreens;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientCommonNetworkHandler;
+import net.minecraft.client.network.ClientConnectionState;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.network.listener.ClientPlayPacketListener;
+import net.minecraft.network.listener.TickablePacketListener;
+import net.minecraft.network.packet.s2c.play.CloseScreenS2CPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayNetworkHandler.class)
+public abstract class ScreenCloseMixin extends ClientCommonNetworkHandler implements ClientPlayPacketListener, TickablePacketListener {
+    protected ScreenCloseMixin(MinecraftClient client, ClientConnection connection, ClientConnectionState connectionState) {
+        super(client, connection, connectionState);
+    }
+
+    @Inject(method = "onCloseScreen", at = @At("HEAD"), cancellable = true)
+    private void onCloseScreen(CloseScreenS2CPacket packet, CallbackInfo ci) {
+        if (IgnoreCloseScreens.INSTANCE.shouldIgnore()) ci.cancel();
+    }
+
+}
--- src/main/kotlin/dev/wekend/mop/events/ChatEvents.kt
+++ src/main/kotlin/dev/wekend/mop/events/ChatEvents.kt
@@ -2,17 +2,16 @@ package dev.wekend.mop.events
 
 import dev.wekend.mop.Mop
 import dev.wekend.mop.config.MopSettings
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.minecraft.client.MinecraftClient
-import net.minecraft.client.gui.hud.ChatHud
 import net.minecraft.client.gui.screen.ChatScreen
 import net.minecraft.text.ClickEvent
 import net.minecraft.text.PlainTextContent
 import net.minecraft.text.Text
 
 object ChatEvents {
-
     fun onGameMessage(message: Text) {
-        if (!MopSettings.Companion.chatAutoOpen.get()) return
+        if (!MopSettings.chatAutoOpen.get()) return
 
         val prevInput: String? = message.siblings
             .firstNotNullOfOrNull { sibling ->
@@ -20,13 +19,37 @@ object ChatEvents {
                 if (isPrevious) (sibling.style.clickEvent as? ClickEvent.SuggestCommand)?.command
                 else null
             }
-
         if (prevInput == null) return
-        Mop.LOGGER.info("prevInput: $prevInput")
 
-        // open chat window with previous input
         val client = MinecraftClient.getInstance()
-        client.setScreen(ChatScreen(prevInput, false))
+
+        // Set up tick window to ignore server's close screen packet
+        IgnoreCloseScreens.startIgnoring(2)
+        ClientTickEvents.END_WORLD_TICK.register { IgnoreCloseScreens.tick() }
+
+        val chatScreen = if (
+            MopSettings.chatIncludePrevious.get()
+        ) {
+            ChatScreen(prevInput, true)
+        } else {
+            ChatScreen("", false)
+        }
+
+        client.setScreen(chatScreen)
     }
 
+}
+
+object IgnoreCloseScreens {
+    private var ignoreTicks = 0
+
+    fun tick() {
+        if (ignoreTicks > 0) ignoreTicks--
+    }
+
+    fun startIgnoring(ticks: Int) {
+        ignoreTicks = ticks
+    }
+
+    fun shouldIgnore(): Boolean = ignoreTicks > 0
 }
