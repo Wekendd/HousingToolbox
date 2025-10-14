53d99df22231025fb755774ee06101fc7682e0dd
Wekend <wekendmc@gmail.com>
2025-10-14 16:48:08 -0700

    Undo

--- src/main/kotlin/dev/wekend/housingtoolbox/keybind/KeyNav.kt
+++ src/main/kotlin/dev/wekend/housingtoolbox/keybind/KeyNav.kt
@@ -17,13 +17,13 @@ object KeyNav {
                 KeySpec(
                     id = "search",
                     key = GLFW.GLFW_KEY_F,
-                    modifiers = KeyModifiers(false, true, false), // Ctrl+F
+                    modifiers = KeyModifiers(false, true, false),
                     onPress = { MenuUtils.clickMenuSlot(MenuItems.SEARCH, MenuItems.FILTER) }
                 ),
                 KeySpec(
                     id = "copy",
                     key = GLFW.GLFW_KEY_C,
-                    modifiers = KeyModifiers(false, true, false), // Ctrl+F
+                    modifiers = KeyModifiers(false, true, false),
                     onPress = {
                         clickMenuTargets(
                             MenuUtils.Target(MenuItems.COPY, 0),
@@ -34,13 +34,19 @@ object KeyNav {
                 KeySpec(
                     id = "paste",
                     key = GLFW.GLFW_KEY_V,
-                    modifiers = KeyModifiers(false, true, false), // Ctrl+F
+                    modifiers = KeyModifiers(false, true, false),
                     onPress = {
                         clickMenuTargets(
                             MenuUtils.Target(MenuItems.PASTE, 0),
                             MenuUtils.Target(MenuItems.COPY, 1)
                         )
                     }
+                ),
+                KeySpec(
+                    id = "undo",
+                    key = GLFW.GLFW_KEY_Z,
+                    modifiers = KeyModifiers(false, true, false),
+                    onPress = { MenuUtils.clickMenuSlot(MenuItems.UNDO) }
                 ),
                 KeySpec(
                     id = "back",
@@ -66,6 +72,7 @@ object KeyNav {
         val SEARCH = MenuUtils.MenuSlot(Items.COMPASS, "Search")
         val FILTER = MenuUtils.MenuSlot(Items.OAK_SIGN, "Filter")
         val COPY = MenuUtils.MenuSlot(Items.WRITABLE_BOOK, "Copy Actions")
+        val UNDO = MenuUtils.MenuSlot(Items.CAULDRON, "Restore Last Removed Action")
         val PASTE = MenuUtils.MenuSlot(Items.BOOK, "Paste Actions")
         val BACK = MenuUtils.MenuSlot(Items.ARROW, "Go Back")
         val CLOSE = MenuUtils.MenuSlot(Items.BARRIER, "Close")
