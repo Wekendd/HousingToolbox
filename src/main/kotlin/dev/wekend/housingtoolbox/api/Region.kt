f160feb22c71df3aa060c6a410971b436b14b2e5
Wekend <wekendmc@gmail.com>
2025-11-21 14:33:36 -0800

    RegionImporter

--- src/main/kotlin/dev/wekend/housingtoolbox/api/Region.kt
+++ src/main/kotlin/dev/wekend/housingtoolbox/api/Region.kt
@@ -1,12 +1,20 @@
 package dev.wekend.housingtoolbox.api
 
 import dev.wekend.housingtoolbox.feature.data.Action
+import dev.wekend.housingtoolbox.util.MenuUtils.MenuSlot
 
 interface Region {
     var name: String
 
+    // Region must be selected to run this.
+    suspend fun createIfNotExists(): Boolean
+
     suspend fun getName(): String = name
     suspend fun setName(newName: String)
+
+    suspend fun teleportToRegion()
+
+    suspend fun moveRegion()
 
     suspend fun getPvpSettings(): MutableMap<PvpSettings, Boolean>
     suspend fun setPvpSettings(newPvpSettings: MutableMap<PvpSettings, Boolean>)
@@ -19,17 +27,17 @@ interface Region {
 
     suspend fun delete()
 
-    enum class PvpSettings {
-        PVP,
-        DOUBLE_JUMP,
-        FIRE_DAMAGE,
-        FALL_DAMAGE,
-        POISON_WITHER_DAMAGE,
-        SUFFOCATION,
-        HUNGER,
-        NATURAL_REGENERATION,
-        DEATH_MESSAGES,
-        INSTANT_RESPAWN,
-        KEEP_INVENTORY,
+    enum class PvpSettings(val item: MenuSlot) {
+        PVP(MenuSlot(null, "PvP/Damage")),
+        DOUBLE_JUMP(MenuSlot(null, "Double Jump")),
+        FIRE_DAMAGE(MenuSlot(null, "Fire Damage")),
+        FALL_DAMAGE(MenuSlot(null, "Fall Damage")),
+        POISON_WITHER_DAMAGE(MenuSlot(null, "Poison/Wither Damage")),
+        SUFFOCATION(MenuSlot(null, "Suffocation")),
+        HUNGER(MenuSlot(null, "Hunger")),
+        NATURAL_REGENERATION(MenuSlot(null, "Natural Regeneration")),
+        DEATH_MESSAGES(MenuSlot(null, "Death Messages")),
+        INSTANT_RESPAWN(MenuSlot(null, "Instant Respawn")),
+        KEEP_INVENTORY(MenuSlot(null, "Keep Inventory")),
     }
 }
